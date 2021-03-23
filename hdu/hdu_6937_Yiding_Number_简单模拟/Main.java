7675cbf191a69b91666a1842943e2fd6c2ec775d
ncfxy <359419163@qq.com>
2021-03-23 08:41:46 +0800

    Create hdu_6937_Yiding_Number_简单模拟

---
+++ hdu/hdu_6937_Yiding_Number_简单模拟/Main.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        Main m = new Main();
+        m.init();
+    }
+
+    public void init() {
+        Queue<Integer> q = new LinkedList<Integer>();
+        Map<Integer, Boolean> m = new HashMap<Integer, Boolean>();
+        q.add(1);
+        m.put(1, true);
+        while(!q.isEmpty()) {
+            Integer value = q.remove();
+            m.put(value, true);
+            int a[] = {1,2,3,4};
+            a[0] = 5 * value + 13;
+            a[1] = 13 * value + 5;
+            a[2] = 5 * value - 13;
+            a[3] = 13 * value - 5;
+            for (int i = 0; i < 4;i++) {
+                Boolean b = m.get(a[i]);
+                if (a[i] > 0 && a[i] <= 1000000 && b == null) {
+                    q.add(a[i]);
+                    m.put(a[i], true);
+                }
+            }
+        }
+
+        Scanner cin = new Scanner(System.in);
+        while(cin.hasNextInt()) {
+            int t = cin.nextInt();
+            for (int i = 0;i < t; i++) {
+                int n = cin.nextInt();
+                Boolean b = m.get(n);
+                if (b != null) {
+                    System.out.println("Yes");
+                } else {
+                    System.out.println("No");
+                }
+            }
+        }
+    }
+}
